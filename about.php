225f4c2d5bfb9c14ffe6c9741e6ae0cc42aa2205
param <paramjani310@gamil.com>
2022-04-07 11:50:17 +0530

    website

---
+++ about.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  
+</head>
+<body>
+ 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Balaji Laboratory</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Hair Oil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contect</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>Balaji Laboratory</h1>
+</div>
+
+<section class="my-5">
+    <div class="dy-5">
+          <h3 class="text-center">About Us</h3>
+    </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-12">
+        <img src="img/c.jpg" class="img-fluid aboutimg"> 
+      </div> 
+      <div class="col-lg-6 col-md-6 col-12">
+        <h2 class="display-4">Balaji Laboratory</h2>
+        <p class="py-3">Balaji Laboratory beaing all type of cosmetic products like Amla,Coconut,Jasmine,Aritha,Duthi,Almond,Himgiri Hair olis for soft and shiny hair.Balaji Laboratory beaing Heena with Shikakai Shampoo,Orange Shampoo with extra conditioner,Aloevera,Amla shampoo.Balaji Laboratory provides best cocmetic products..
+        </p>
+        <a href="about.php" class="btn btn-success">check more</a>
+      </div> 
+     </div>
+    </div>
+</section>
+
+<footer>
+   <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <p class="p-3 bg-dark text-white text-center">
+      <i class='fas fa-map-marker-alt'></i> Golden industrial estate n0.2 plot no D/14 Vivra,<br>Rajkot 360311<br><i class='fas fa-envelope-square'></i> Balajilaboratory01@gmail.com</p>
+</footer>
+
+</body>
+
+</html>
